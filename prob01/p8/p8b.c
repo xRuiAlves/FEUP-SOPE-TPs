b8066a54992b822006efc5348d6b482dedd65955
xRuiAlves <up201606746@fe.up.pt>
2018-02-19 21:43:33 +0000

    Finished problem 8 from Exercise Sheet #1

--- prob01/p8/p8b.c
+++ prob01/p8/p8b.c
@@ -1,12 +1,23 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
+#include <sys/time.h>
+#include <sys/resource.h>
 
 #define INTERVAL_EXTREME_INDEX 1
 #define TERMINATION_VALUE_INDEX 2
 
 int main(int argc , char* argv[]){
-	clock_t ti = clock();
+	struct timeval utime_i , stime_i , utime_f , stime_f;
+	struct timespec rtime_i , rtime_f;
+	struct rusage res_usage;
+
+	// Measure initial time
+	getrusage(RUSAGE_SELF , &res_usage);
+	clock_gettime(CLOCK_REALTIME , &rtime_i);
+	stime_i = res_usage.ru_stime;
+	utime_i = res_usage.ru_utime;
+
 
 	if (argc != 3){
 		printf("usage: p8a n1 n2\n\tn1  Interval superior extreme\n\tn2  Final number termination\n");
@@ -31,12 +42,17 @@ int main(int argc , char* argv[]){
 		printf("%d\t" , random_number);
 	} while(random_number != termination_val);
 
-	clock_t tf = clock();
 
-	double run_time = (double)(tf - ti) / CLOCKS_PER_SEC;
+	// Measure final time
+	getrusage(RUSAGE_SELF , &res_usage);
+	clock_gettime(CLOCK_REALTIME , &rtime_f);
+	stime_f = res_usage.ru_stime;
+	utime_f = res_usage.ru_utime;
 	
-	printf("\nTotal program time: %lf\n" , run_time );
-	
+	printf("\n\nReal time: %f" , (rtime_f.tv_sec - rtime_i.tv_sec)*1000.0 + (rtime_f.tv_nsec - rtime_i.tv_nsec)/1000000.0);
+	printf("\nSystem time: %f" , (stime_f.tv_sec - stime_i.tv_sec)*1000.0 + (stime_f.tv_usec - stime_i.tv_usec)/1000.0);
+	printf("\nUser time: %f\n" , (utime_f.tv_sec - utime_i.tv_sec)*1000.0 + (utime_f.tv_usec - utime_i.tv_usec)/1000.0);
 
 	return 0;
+
 }
